84ff7bda32b2e6a828a87fba2d146900ebc81ace
Adrian Ng <beaconwarden@outlook.com>
2017-03-23 10:04:56 +0000

    patition

--- array.js
+++ array.js
@@ -24,3 +24,23 @@ function count (ele, array){
     return counter ;
     
 }
+
+function partitionLayout (array , n){
+    var tDarray = [];
+    for (var i = 0 ;  i < array.length ; i ++) {
+          var tempArray = [] ; 
+         if (i<array.length){
+				tempArray.push(array[i]); tempArray.push(array[i+1])	;
+				 tDarray.push(tempArray);
+		}
+		 
+		 else  console.log('exeeeced');
+	}
+		var layout ="";
+	for (var i = 0 ; i <n ; i++ ){
+		 for (var j = 0 ; j <tDarray[0][j] ; j++)
+         {layout += tDarray[0][j] + "  | " ; }
+		console.log (layout);
+	}
+
+}
