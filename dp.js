1c7421391c36a7363747a7a4e0da63f27f5f96f4
Adrian Ng <beaconwarden@outlook.com>
2017-05-07 10:12:33 +0000

    f dp

--- dp.js
+++ dp.js
@@ -30,10 +30,10 @@ function  fibTopDown( n) {
 //Bottom-Up solution for Fibonacci Series:
 
 function fibDP( x) {
-		int fib[] = new int[x + 1];
+		var fib=[] ;
 		fib[0] = 0;
 		fib[1] = 1;
-		for (int i = 2; i < x + 1; i++) {
+		for (var i = 2; i <x+1; i++) {
 			fib[i] = fib[i - 1] + fib[i - 2];
 		}
 		return fib[x];
