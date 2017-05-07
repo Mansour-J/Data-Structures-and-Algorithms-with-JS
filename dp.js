9230ff38984632ab8838adcea667356dbe6d1778
Adrian Ng <beaconwarden@outlook.com>
2017-05-07 10:02:10 +0000

    f dp

--- dp.js
+++ dp.js
@@ -1,3 +1,5 @@
+//https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e
+
 function fibonacci(num, memo) {
   memo = memo || {};
    console.log(memo) ;
@@ -8,4 +10,31 @@ function fibonacci(num, memo) {
 }      ; 
 
 fibonacci(9);
-//https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e
+
+
+
+//http://algorithms.tutorialhorizon.com/introduction-to-dynamic-programming-fibonacci-series/  
+//Top-Down Approach: 
+function  fibTopDown( n) {
+		if(n==0) return 1;
+		if(n==1) return 1;
+		if(fib[n]!=0){
+			return fib[n];
+		}else{
+			fib[n] = fibTopDown(n-1) + fibTopDown(n-2);
+			return fib[n];
+		}
+	}
+
+
+//Bottom-Up solution for Fibonacci Series:
+
+function fibDP( x) {
+		int fib[] = new int[x + 1];
+		fib[0] = 0;
+		fib[1] = 1;
+		for (int i = 2; i < x + 1; i++) {
+			fib[i] = fib[i - 1] + fib[i - 2];
+		}
+		return fib[x];
+	}
