ced13789d47777b40c25a2bfb2a38dcb111e2432
Adrian Ng <beaconwarden@outlook.com>
2017-05-07 09:08:51 +0000

    quick mergesort

--- Sorting.js
+++ Sorting.js
@@ -197,5 +197,55 @@ function shellSort( array) {
 	
 };
 
-console.log(shellSort([100,99,98,97,96,95,94,93,92,91,90,89,88,87,86,85,84,83,82,81,80,79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60,59,58,57,56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]));
+//console.log(shellSort([100,99,98,97,96,95,94,93,92,91,90,89,88,87,86,85,84,83,82,81,80,79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60,59,58,57,56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]));
 
+
+function  swap( num1,  num2, intArray) {
+   var temp = intArray[num1];
+   intArray[num1] = intArray[num2];
+   intArray[num2] = temp;
+}
+
+
+function QuickSort  ( left,  right,intArray){
+	
+   if(right-left <= 0) {
+      return;   
+   } else {
+      var pivot = intArray[right];
+      var partitionPoint = partition(left, right, pivot,intArray);
+      QuickSort(left,partitionPoint-1,intArray);
+      QuickSort(partitionPoint+1,right,intArray);
+   }  
+
+  return intArray ;
+};
+
+function partition( left,  right,  pivot,intArray) {
+   var leftPointer = left -1;
+   var rightPointer = right;
+
+   while(true) {
+      while(intArray[++leftPointer] < pivot) {
+         //do nothing
+      }
+		
+      while(rightPointer > 0 && intArray[--rightPointer] > pivot) {
+         //do nothing
+      }
+
+      if(leftPointer >= rightPointer) {
+         break;
+      } else {
+         console.log(" item swapped :%d,%d\n"+ intArray[leftPointer]+intArray[rightPointer]);
+         swap(leftPointer,rightPointer,intArray);
+      }
+   }
+	
+   console.log(" pivot swapped :%d,%d\n"+ intArray[leftPointer]+intArray[right]);
+   swap(leftPointer,right,intArray);
+   console.log("Updated Array: "); 
+   return leftPointer;
+};
+
+QuickSort(0,[4,6,3,2,1,9,7].length-1 , [4,6,3,2,1,9,7]);
